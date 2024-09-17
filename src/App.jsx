30b06c074b48df377b4287ea82e48798c0ba69a7
Maryam Tehrani <maryamtehranizadeh@yahoo.com>
2024-09-17 17:16:14 +0100

    review

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Search />
+      <h1>Book App</h1>
+    </>
+  );
+}
+
+export default App;
