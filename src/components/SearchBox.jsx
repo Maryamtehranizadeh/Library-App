8e22125fe3ed5a446e08e941d2cf0976f6095e50
Maryam Tehrani <maryamtehranizadeh@yahoo.com>
2024-09-22 14:37:38 +0100

    css

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,12 +1,14 @@
 import { SlMagnifier } from "react-icons/sl";
+import styles from "./SearchBox.module.css";
 
 function SearchBox({ search, setSearch, searchHandler }) {
   return (
-    <div>
+    <div className={styles.search}>
       <input
+        onKeyDown={(e) => e.keyCode === 13 && searchHandler()}
         onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
         type="text"
-        placeholder="Search"
+        placeholder="Search the title here"
         value={search}
       />
       <button onClick={searchHandler}>
